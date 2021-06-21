091a4fdb8b7caacbcf1bcd1d00bdd2dad332b869
franklynkleiton <franklynkleiton18@gmail.com>
2021-06-21 09:25:46 -0300

    refactor: improve about animation

--- sections/About/index.tsx
+++ sections/About/index.tsx
@@ -1,16 +1,27 @@
+import { useEffect } from 'react';
 import styled from "styled-components";
-import { motion, useViewportScroll, useTransform } from 'framer-motion';
+import { motion, useAnimation } from 'framer-motion';
 
 import { Container, Headline3 as H3 } from "../../styles/Components";
 
 export default function About() {
-  const { scrollYProgress } = useViewportScroll();
-  const opacity = useTransform(scrollYProgress, [0.6, 0.9], [0, 1]);
-  const y = useTransform(scrollYProgress, [0.7, 0.9], [100, 0]);
+  const controls = useAnimation();
+
+  useEffect(() => {
+    function handleScroll() {
+      if (+this.scrollY > window.innerHeight / 2) {
+        controls.start({
+          opacity: 1,
+          y: 0
+        }, { duration: .5 });
+      }
+    }
+    window.addEventListener('scroll', handleScroll);
+  }, []);
 
   return (
     <Container>
-      <Presentation style={{ opacity, position: 'absolute', top: y }}>
+      <Presentation initial={{ y: -50 }} animate={controls}>
         <Rect />
         <Headline3>
           Global Coding Labs is a software development start up known for build
@@ -33,11 +44,11 @@ const Presentation = styled(motion.div)`
   max-height: 8em;
   height: 100%;
   align-items: center;
+  opacity: 0;
 `;
 
 const Rect = styled.div`
   background-color: ${props => props.theme.secondary1};
   width: .5em;
   height: 100%;
-
 `;
